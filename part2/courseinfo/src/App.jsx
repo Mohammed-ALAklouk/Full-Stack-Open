1f5d83b8a269a9b09b4899d0162c1af999315679
Mohammed-ALAklouk <moha.alaklouk@gmail.com>
2026-07-23 18:08:25 +0300

    Solved exercise 2.2

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -6,6 +6,11 @@ const Content = ({ course }) => {
       {course.parts.map(part => (
         <Part key={part.id} part={part} />
       ))}
+      <p> 
+        <strong>
+          Total of {course.parts.reduce((sum, part) => sum + part.exercises, 0)} exercises
+        </strong>
+      </p>
     </>
   )
 }
@@ -38,6 +43,11 @@ const App = () => {
         name: 'State of a component',
         exercises: 14,
         id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
       }
     ]
   }
