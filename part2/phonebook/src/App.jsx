75d6c5b30e9365ad72ac82cf50823633f265d779
Mohammed-ALAklouk <moha.alaklouk@gmail.com>
2026-07-25 15:21:03 +0300

    Solved exercise 2.12

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -70,10 +70,13 @@ const App = () => {
       number: newNumber,
       id: persons.length + 1
     }
-    setPersons(persons.concat(personObject))
-    setNewName('')
-    setNewNumber('')
-  }
+
+    axios.post('http://localhost:3001/persons', personObject).then(response => {
+      setPersons(persons.concat(response.data))
+      setNewName('')
+      setNewNumber('')
+    })
+    }
 
   const personsToShow = filter === ''
     ? persons
